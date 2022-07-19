e29ab9555c377c75fdf1acd18bc94239f50a854f
Yahor Lukyanchyk <yahorlukyanchyk@gmail.com>
2022-07-19 16:55:10 +0300

    burger menu fix and second section was added

--- scripts/burgerMenu.js
+++ scripts/burgerMenu.js
@@ -6,9 +6,11 @@ function getViewportHeight (){
 }
 
 document.querySelector(".burger-menu").addEventListener("click", () => {
-  burgerNav.style.top = 0;
+  document.body.style.overflow = "hidden"
+  burgerNav.style.left = 0;
 });
 
 document.querySelector(".burger-menu__exit").addEventListener("click", () => {
-  burgerNav.style.top = -100 + "%";
+  document.body.style.overflow = "auto"
+  burgerNav.style.left = -100 + "%";
 });
